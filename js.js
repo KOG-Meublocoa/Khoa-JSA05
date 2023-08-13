8f7376196a55ba036e528110515380d51a9e179c
KOG-Meublocoa <duongdangkhoa071@gmail.com>
2023-08-13 12:03:29 +0700

    Buổi 2

---
+++ js.js
@@ -0,0 +1,57 @@
+// let x = 0;
+// while (x<10){
+//     console.log(x += 1);
+// }
+// let x = 0
+// do {
+//     console.log(x++);
+   
+// } while ( x<10);
+
+// var const let
+
+// let i =6 ;
+
+// while(i<5){
+//     console.log(i)
+//     i++
+// }
+
+// let a = 5
+
+// do {
+//     console.log(a);
+// } while (a >6);
+
+// let n=0
+// for(n=0;n<5;n++){
+// console.log(n)
+// }
+
+let MyNumber = [0,1,2];
+let MyString = ['Ngoc','Lam','tai']
+
+
+
+console.log(MyString);
+console.log(MyNumber);
+
+// for(i=0;i<3;i++){
+// console.log(i +":"+MyString[i]);
+// }
+
+// for(i=0;i<3;i++)
+//     alert(i +":"+MyString[i]);
+
+// let i=0
+// while (i<3) {
+//     console.log(i + ":" + MyString);
+//     i++;
+// }
+
+let i=0
+do {
+    console.log(i + ":" + MyString[i]);
+    i++;
+} while (i<3);
+    
